1e9107509c8fa661a040f918876f0eadc871e829
MikyProchy <mikulas.j.prochazka@proton.me>
2025-11-29 22:59:01 +0100

    fixed build

--- helpers/getListById.ts
+++
@@ -1,7 +0,0 @@
-import { ListDto } from "@/types/listTypes";
-import { todoLists } from "@/data/todoLists";
-
-export const getListById = (id?: string): ListDto | null => {
-  if (!id) return null;
-  return todoLists.find((list) => list.id === id) ?? null;
-};
